e9bf8d1bdf9f40d9751abcf21370970321c44fe9
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-10-10 22:36:18 +0300

    Finished ?.

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Memory Flip Game
+
+This is a simple and fun **Memory Flip Game** where you can test your memory by matching pairs of cards. The game features a 4x5 grid of cards, and your goal is to find all the matching pairs.
+
+## How to Play
+
+1. Download the game files and open the `index.html` file in your browser.
+2. Click on any two cards to flip them over.
+   - If the cards **match**, they will stay flipped.
+   - If they **don't match**, they will flip back after a short delay.
+3. The goal is to find all the matching pairs in the least number of tries.
+
+Once you've matched all the cards, you can restart the game for another round of fun! :)
+
+You can play it on https://mohammedmmc.github.io/Memory-Flip-Game
---
+++ script.js
@@ -0,0 +1,64 @@
+let IMAGES = [
+    "/icons/c-logo.png",
+    "/icons/html-5.png",
+    "/icons/js.png",
+    "/icons/mysql.png",
+    "/icons/php.png",
+    "/icons/physics.png",
+    "/icons/python.png",
+    "/icons/technology.png",
+    "/icons/typescript.png",
+    "/icons/visual-basic.png",
+];
+
+
+window.onload = () => {
+    const gameElement = document.getElementById("game");
+
+    IMAGES.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < (4 * 5) / 2; i++) {
+        gameElement.insertAdjacentHTML("beforeend", `<div class="card" onclick="csc(this);" style="--imgurl: url(${IMAGES[i]});"></div>`);
+    }
+    IMAGES.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < (4 * 5) / 2; i++) {
+        gameElement.insertAdjacentHTML("beforeend", `<div class="card" onclick="csc(this);" style="--imgurl: url(${IMAGES[i]});"></div>`);
+    }
+}
+
+let points = 0;
+let tries = 0;
+let selectedCards = [];
+
+function checkWin() {
+    if (points !== (4 * 5) / 2)return;
+    const gameElement = document.getElementById("game");
+    gameElement.classList.add("finished");
+}
+
+/** @param {HTMLElement} card */
+function csc(card) {
+    if (selectedCards.includes(card) || card.disabled == true) return;
+    selectedCards.push(card);
+    card.classList.add("flipped");
+
+
+    if (selectedCards.length >= 2) {
+        if (getComputedStyle(selectedCards[0]).getPropertyValue('--imgurl') === getComputedStyle(selectedCards[1]).getPropertyValue('--imgurl')) {
+            selectedCards = [];
+            points++;
+            const triesTitle = document.getElementById("tries");
+            triesTitle.innerText = "Tries: " + tries;
+        } else {
+            const gameElement = document.getElementById("game");
+            [...gameElement.children].forEach(c => c.disabled = true);
+            setTimeout(() => {
+                selectedCards.forEach(c => c.classList.remove("flipped"));
+                selectedCards = [];
+                [...gameElement.children].forEach(c => c.disabled = false);
+            }, 1000)
+        }
+        tries++;
+    }
+
+    checkWin();
+}
